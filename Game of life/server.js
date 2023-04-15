fcdeac6bd5321a3350d5884b5d281b80559db082
MkrtichHunanyan <mkrtich.hunanyan.g@tumo.org>
2023-04-15 12:46:14 +0400

    Start server

--- Game of life/server.js
+++ Game of life/server.js
@@ -1,1 +1,16 @@
-var express = require()
+var express = require("express")
+var app = express()
+var server = require('http').Server(app)
+var io = require('socket.io')(server)
+var fs = require('fs')
+
+app.use(express.static("."))
+
+app.get('/',function(req,res) {
+    res.redirect("index.html")
+})
+
+server.listener(3000,function(){
+    console.log("Working");
+})
+
