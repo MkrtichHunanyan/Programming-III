b0f17aa4e76af2490c3fc587d963744797027f22
MkrtichHunanyan <129868495+MkrtichHunanyan@users.noreply.github.com>
2023-05-06 18:42:17 +0400

    Upd water.js

--- Game of life/water.js
+++ Game of life/water.js
@@ -4,18 +4,7 @@ module.exports = class Water extends LivingCreature{
     constructor(x,y){
         super(x, y)
         this.lifeTime = 0
-        this.directions = [
-            // [this.x - 1, this.y - 1],
-            // [this.x    , this.y - 1],
-            // [this.x + 1, this.y - 1],
-            // [this.x - 1, this.y    ],
-            // [this.x + 1, this.y    ],
-            // [this.x - 1, this.y + 1],
-            // [this.x    , this.y + 1],
-            // [this.x + 1, this.y + 1]
-        ];
-    
-    }
+}
 
     fill(){
         if(this.lifeTime <= 30){
